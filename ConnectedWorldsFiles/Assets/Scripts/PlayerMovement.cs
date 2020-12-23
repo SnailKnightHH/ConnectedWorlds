04fbcf8ce12964377bdc1d7a2d22110d92413f92
Sicheng Yi <15852786007ysc@gmail.com>
2020-12-23 16:50:47 -0500

    Added basic player movement

---
+++ ConnectedWorldsFiles/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 3f;
+
+    private Rigidbody2D rb;
+    private float horizontalMovement;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        getInputs();
+    }
+
+
+    private void FixedUpdate()
+    {
+        movePlayer();
+    }
+
+    private void getInputs()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+    }
+    private void movePlayer()
+    {
+        Vector3 targetVelocity = new Vector2(horizontalMovement * movementSpeed, rb.velocity.y);
+        rb.velocity = targetVelocity;
+    }
+}
