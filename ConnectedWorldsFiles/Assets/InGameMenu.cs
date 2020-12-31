6581b049ee889cb8f3994acc6462c046d64e34d9
David Hu <huyangming2016@gmail.com>
2020-12-31 16:14:12 -0500

    Back Story UI Update

---
+++ ConnectedWorldsFiles/Assets/InGameMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameMenu : MonoBehaviour
+{
+    public static bool isGamePause = false;
+    public GameObject Panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Panel.SetActive(false);
+    }
+
+    public void Pause()
+    {
+        Panel.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePause = true;
+    }
+
+    public void Resume()
+    {
+        Panel.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePause = false;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ ConnectedWorldsFiles/Assets/PauseMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : InGameMenu
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGamePause = false;
+    }
+
+    // Update is called once per frame
+/*    void Update()
+    {
+        Pausemenu();
+    }
+
+    private void Pausemenu()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePause) Resume();
+            else Pause();
+        }
+    }*/
+}
