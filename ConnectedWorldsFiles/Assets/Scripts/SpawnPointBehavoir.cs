ffbb618184e3a305b93ab40462024e950209aada
David Hu <huyangming2016@gmail.com>
2021-01-02 12:02:41 -0500

    Spawn Point UI Update

--- ConnectedWorldsFiles/Assets/Scripts/SpawnPointBehavoir.cs
+++ ConnectedWorldsFiles/Assets/Scripts/SpawnPointBehavoir.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SpawnPointBehavoir : MonoBehaviour
 {
     private SceneManager sceneManager;
+    private TextMeshProUGUI textMeshProUGUI;
+
 
     private void Awake()
     {
         sceneManager = FindObjectOfType<SceneManager>();
+        textMeshProUGUI = GetComponentInChildren<TextMeshProUGUI>();
+        textMeshProUGUI.alpha = 0f;
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
-        sceneManager.SetSpawnPoint(transform);
+        {
+            sceneManager.SetSpawnPoint(transform);
+            StartCoroutine(displayText());
+        }
+       
+    }
+
+    private IEnumerator displayText()
+    {
+        textMeshProUGUI.alpha = 1f;
+        yield return new WaitForSeconds(3f);
+        textMeshProUGUI.alpha = 0f;
     }
 }
